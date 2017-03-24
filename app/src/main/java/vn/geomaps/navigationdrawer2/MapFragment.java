1810284c74a4d1735c608e7876a9f1f655d6a387
Thien Phu <lethienphu.1404@gmail.com>
2017-03-24 14:31:54 +0700

    first commit, add google map

---
+++ app/src/main/java/vn/geomaps/navigationdrawer2/MapFragment.java
@@ -0,0 +1,36 @@
+package vn.geomaps.navigationdrawer2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.InflateException;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by lionf_000 on 23-Mar-17.
+ */
+
+public class MapFragment extends Fragment {
+    private static View view;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //View view = inflater.inflate(R.layout.fragment_map,container,false);
+
+        if(view != null){
+            ViewGroup parent = (ViewGroup) view.getParent();
+            if(parent != null){
+                parent.removeView(view);
+            }
+        }
+        try{
+            view = inflater.inflate(R.layout.fragment_map,container,false);
+        }catch (InflateException e){
+
+        }
+
+        return view;
+    }
+}
---
+++ app/src/main/java/vn/geomaps/navigationdrawer2/NewsFragment.java
@@ -0,0 +1,22 @@
+package vn.geomaps.navigationdrawer2;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by lionf_000 on 23-Mar-17.
+ */
+
+public class NewsFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_news,container,false);
+
+        return view;
+    }
+}
